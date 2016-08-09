8145be11278f4ec6fb54f4064172531bbc6ec930
Abdullah Bashir <mabdullah353@gmail.com>
2016-08-09 19:36:43 +0500

    Dropping fat arrow notation to support iojs version

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -5,39 +5,39 @@ let route = require('../lib/routes.js')
 let chai = require('chai')
 let expect = chai.expect
 
-describe('Routes', () => {
-  describe('#get()', () => {
-    it('Method is availble', () => {
+describe('Routes', function () {
+  describe('#get()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('get')).to.equal(true)
     })
   })
 
-  describe('#post()', () => {
-    it('Method is availble', () => {
+  describe('#post()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('post')).to.equal(true)
     })
   })
 
-  describe('#put()', () => {
-    it('Method is availble', () => {
+  describe('#put()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('put')).to.equal(true)
     })
   })
 
-  describe('#delete()', () => {
-    it('Method is availble', () => {
+  describe('#delete()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('delete')).to.equal(true)
     })
   })
 
-  describe('#head()', () => {
-    it('Method is availble', () => {
+  describe('#head()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('head')).to.equal(true)
     })
   })
 
-  describe('#patch()', () => {
-    it('Method is availble', () => {
+  describe('#patch()', function () {
+    it('Method is availble', function () {
       expect(route.hasOwnProperty('patch')).to.equal(true)
     })
   })
